04063e18b1d4596eb3b5d23220efb17d661a424f
Kevin Litvin <ktl13@sfu.ca>
2023-03-13 00:30:19 -0700

    Updates

---
+++ precache-manifest.a7f0d28234905591df2b68d3e69fa6d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f1d6df03a087b75c62af586d276ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac050ce3bd8d3ef6b6f1",
+    "url": "/static/css/main.1e3bf634.chunk.css"
+  },
+  {
+    "revision": "dab575be2ac0398ea75d",
+    "url": "/static/js/2.04150b8f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.04150b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac050ce3bd8d3ef6b6f1",
+    "url": "/static/js/main.b67a395d.chunk.js"
+  },
+  {
+    "revision": "afdb894f831da2e74d33",
+    "url": "/static/js/runtime-main.b4c76d27.js"
+  }
+]);
